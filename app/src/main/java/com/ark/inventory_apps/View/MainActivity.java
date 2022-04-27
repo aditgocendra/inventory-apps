6b53ee3fbee1f91a21924ac230c1cf5ddccdf541
aditgocendra <gocendra123@gmail.com>
2022-04-28 00:17:44 +0700

    handle routes

--- app/src/main/java/com/ark/inventory_apps/View/MainActivity.java
+++ app/src/main/java/com/ark/inventory_apps/View/MainActivity.java
@@ -1,10 +1,15 @@
 package com.ark.inventory_apps.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.os.Handler;
+import com.ark.inventory_apps.Globals.Functions;
+import com.ark.inventory_apps.Globals.Variables;
 import com.ark.inventory_apps.R;
+import com.ark.inventory_apps.View.Auth.Authentication;
+import com.ark.inventory_apps.View.Users.Home;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Functions.checkWindowSetFlag(this);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this::routes, 1000);
+    }
+
+    private void routes(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null){
+            Functions.updateUI(this, Home.class);
+            Variables.uid = user.getUid();
+        }else {
+            Functions.updateUI(this, Authentication.class);
+        }
+        finish();
+
     }
 }
